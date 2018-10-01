a90f197c2f81d0780c8ed11b07b56fe780e2c176
Costin Morariu <cm@interactivemedica.com>
2018-10-01 21:26:45 +0200

    #3 Code cleanup

--- EBikesShop.Ui.Web.Tests/RetailCalculatorAcceptanceTests.cs
+++ EBikesShop.Ui.Web.Tests/RetailCalculatorAcceptanceTests.cs
@@ -27,12 +27,12 @@ namespace EBikesShop.Ui.Web.Tests
         [TestCase(301, 3.12, "TX", 997.815)] 
         [TestCase(99, 1.9, "AL", 195.624)]
         [TestCase(45, 19.99, "CA", 973.762875)]
-        // with discount
-        [TestCase(1, 1000, "UT", 1036.445)]
-        [TestCase(2, 2500, "NV", 5130)]
-        [TestCase(10, 700, "TX", 6916.875)]
-        [TestCase(100, 100, "AL", 9360)]
-        [TestCase(5000, 10, "CA", 46006.25)]
+        // with discount applied before taxes
+        [TestCase(1, 1000, "UT", 1036.445)] // 3%
+        [TestCase(2, 2500, "NV", 5130)] // 5%
+        [TestCase(10, 700, "TX", 6916.875)] // 7%
+        [TestCase(100, 100, "AL", 9360)] // 10%
+        [TestCase(5000, 10, "CA", 46006.25)] // 15%
         public void Can_calculate_total_price(
             int items, decimal pricePerItem, string stateCode, decimal expectedTotalPrice)
         {
