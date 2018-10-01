24f9411ed56931dfc9adaae767a894af02ef135c
Costin Morariu <cm@interactivemedica.com>
2018-10-01 20:57:10 +0200

    #3 Adds discount calculation

--- EBikesShop.Ui.Web.Tests/RetailCalculatorAcceptanceTests.cs
+++ EBikesShop.Ui.Web.Tests/RetailCalculatorAcceptanceTests.cs
@@ -21,12 +21,19 @@ namespace EBikesShop.Ui.Web.Tests
             ApplicationRunner.CloseBrowser();
         }
 
+        // without discount
         [TestCase(2, 50.0, "UT", 106.85)]
         [TestCase(11, 2.5, "NV", 29.7)]
         [TestCase(301, 3.12, "TX", 997.815)] 
         [TestCase(99, 1.9, "AL", 195.624)]
         [TestCase(45, 19.99, "CA", 973.762875)]
-        public void Can_calculate_total_price_without_discount(
+        // with discount
+        [TestCase(1, 1000, "UT", 1036.445)]
+        [TestCase(2, 2500, "NV", 5130)]
+        [TestCase(10, 700, "TX", 6916.875)]
+        [TestCase(100, 100, "AL", 9360)]
+        [TestCase(5000, 10, "CA", 46006.25)]
+        public void Can_calculate_total_price(
             int items, decimal pricePerItem, string stateCode, decimal expectedTotalPrice)
         {
             ApplicationRunner.SetRetailCalculatorInput(items, pricePerItem, stateCode);
